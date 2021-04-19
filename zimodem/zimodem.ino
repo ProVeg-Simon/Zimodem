9bd6d0b68a229a514f10832535fc19db6b1ac949
deBaer <37374038+ProVeg-Simon@users.noreply.github.com>
2021-04-19 02:12:32 +0200

    Update zimodem.ino

--- zimodem/zimodem.ino
+++ zimodem/zimodem.ino
@@ -89,8 +89,8 @@ const char compile_date[] = __DATE__ " " __TIME__;
 # define DEFAULT_PIN_DSR 13
 # define DEFAULT_PIN_DTR 12
 # define DEFAULT_PIN_RI 14
-# define DEFAULT_PIN_RTS 4
-# define DEFAULT_PIN_CTS 5 // is 0 for ESP-01, see getDefaultCtsPin() below.
+# define DEFAULT_PIN_RTS 5
+# define DEFAULT_PIN_CTS 4 // is 0 for ESP-01, see getDefaultCtsPin() below.
 # define DEFAULT_PIN_DCD 2
 # define DEFAULT_FCT FCT_RTSCTS
 # define RS232_INVERTED 1
